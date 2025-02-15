cea8544a8f754c9017cbb7bcc6e309ba9e0d8cd7
Thi Pham <thipham1123@gmail.com>
2025-02-15 18:17:48 +0700

    fetch api -on going

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-import { useState } from "react";
-import Search from "./components/Search";
+import { useEffect, useState } from 'react';
+import Search from './components/Search';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bear ${API_KEY}`,
+  },
+};
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErorrMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErorrMessage('Error fetching movies. Please try again.');
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
-      <div className="pattern"/>
+      <div className="pattern" />
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movie</span> You Will Enjoy Without Hassle</h1>
+          <h1>
+            Find <span className="text-gradient">Movie</span> You Will Enjoy
+            Without Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <h1 className="text-white">{searchTerm}</h1>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
